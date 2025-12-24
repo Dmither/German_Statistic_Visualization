714d927eb1e0413b5a6010aaa4b3723e75521eb2
Dmither <mr.syvko@gmail.com>
2025-12-24 15:40:31 +0100

    Add uk and ru maps

--- bonus.py
+++ bonus.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from choropleth_map import draw_st_choropleth_map
 
 def bonus():
     files = [
@@ -20,10 +21,14 @@ def bonus():
         ), data_list[3], on='State', how="inner"
     )
 
+    bonus_data = pd.read_csv('germany_states_uk_ru.csv')
+
     st.header('Bonus')
 
-    (tab1,) = st.tabs([
+    (tab1,tab2,tab3) = st.tabs([
         'Heatmap',
+        'Ukrainians',
+        'Moskals',
     ])
 
     with tab1:
@@ -38,3 +43,16 @@ def bonus():
             zmax=1
         )
         st.plotly_chart(fig)
+
+    with tab2:
+        ukrainians = bonus_data.sort_values('Ukrainians', ascending=False).set_index('State')[
+            'Ukrainians']
+        st.subheader('Ukrainians (%):')
+        draw_st_choropleth_map(ukrainians, color='Greens', contrast=1, legend_num=5, legend_points=1)
+        st.dataframe(ukrainians)
+    with tab3:
+        moskals = bonus_data.sort_values('Moskals', ascending=False).set_index('State')[
+            'Moskals']
+        st.subheader('Moskals (%):')
+        draw_st_choropleth_map(moskals, color='Reds', contrast=1, legend_num=5, legend_points=1)
+        st.dataframe(moskals)
